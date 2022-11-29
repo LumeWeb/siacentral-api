d002e694005da40932d60ae247f8fd0bcb210f09
Nate Maninger <me@n8m.us>
2022-11-29 09:13:20 -0700

    add sort parameter

--- sia/hosts.go
+++ sia/hosts.go
@@ -31,62 +31,111 @@ type (
 	}
 
 	HostFilter url.Values
+	HostSort   string
 )
 
+var (
+	HostSortDateCreated        HostSort = "date_created"
+	HostSortNetAddress         HostSort = "net_address"
+	HostSortPublicKey          HostSort = "public_key"
+	HostSortAcceptingContracts HostSort = "accepting_contracts"
+	HostSortUptime             HostSort = "uptime"
+	HostSortUploadSpeed        HostSort = "upload_speed"
+	HostSortDownloadSpeed      HostSort = "download_speed"
+	HostSortRemainingStorage   HostSort = "remaining_storage"
+	HostSortTotalStorage       HostSort = "total_storage"
+	HostSortUsedStorage        HostSort = "used_storage"
+	HostSortAge                HostSort = "age"
+	HostSortUtilization        HostSort = "utilization"
+	HostSortContractPrice      HostSort = "contract_price"
+	HostSortStoragePrice       HostSort = "storage_price"
+	HostSortDownloadPrice      HostSort = "download_price"
+	HostSortUploadPrice        HostSort = "upload_price"
+)
+
+// WithAcceptingContracts sets the accepting contracts parameter for the host's query
 func (hf HostFilter) WithAcceptingContracts(accepting bool) {
 	hf["acceptcontracts"] = []string{strconv.FormatBool(accepting)}
 }
 
+// WithOnline sets the online parameter for the host's query
 func (hf HostFilter) WithOnline(online bool) {
 	hf["online"] = []string{strconv.FormatBool(online)}
 }
 
+// WithLimit sets the benchmark parameter for the host's query
 func (hf HostFilter) WithBenchmarked(benchmarked bool) {
 	hf["benchmarked"] = []string{strconv.FormatBool(benchmarked)}
 }
 
+// WithMinAge sets the min age for the host query
+func (hf HostFilter) WithMinAge(age uint64) {
+	hf["minage"] = []string{strconv.FormatUint(age, 10)}
+}
+
+// WithMinUptime sets the min uptime for the host query
 func (hf HostFilter) WithMinUptime(minUptime float64) {
 	hf["minuptime"] = []string{strconv.FormatFloat(minUptime, 'f', -1, 64)}
 }
 
+// WithMinDuration sets the min contract duration for the host query
 func (hf HostFilter) WithMinDuration(minDuration uint64) {
 	hf["minduration"] = []string{strconv.FormatUint(minDuration, 10)}
 }
 
+// WithMinStorage sets the min available storage for the host query
 func (hf HostFilter) WithMinStorage(minStorage uint64) {
 	hf["minstorage"] = []string{strconv.FormatUint(minStorage, 10)}
 }
 
+// WithMinUploadSpeed sets the min upload speed for the host query
 func (hf HostFilter) WithMinUploadSpeed(minUploadSpeed uint64) {
 	hf["minuploadspeed"] = []string{strconv.FormatUint(minUploadSpeed, 10)}
 }
 
+// WithMinDownloadSpeed sets the min download speed for the host query
 func (hf HostFilter) WithMinDownloadSpeed(minDownloadSpeed uint64) {
 	hf["mindownloadspeed"] = []string{strconv.FormatUint(minDownloadSpeed, 10)}
 }
 
+// WithMaxStoragePrice sets the max storage price for the host query
 func (hf HostFilter) WithMaxStoragePrice(price types.Currency) {
 	hf["maxstorageprice"] = []string{price.String()}
 }
 
+// WithMaxUploadPrice sets the max upload price for the host query
 func (hf HostFilter) WithMaxUploadPrice(price types.Currency) {
 	hf["maxuploadprice"] = []string{price.String()}
 }
 
+// WithMaxDownloadPrice sets the max download price for the host query
 func (hf HostFilter) WithMaxDownloadPrice(price types.Currency) {
 	hf["maxdownloadprice"] = []string{price.String()}
 }
 
+// WithMaxContractPrice sets the max contract price for the host query
 func (hf HostFilter) WithMaxContractPrice(price types.Currency) {
 	hf["maxcontractprice"] = []string{price.String()}
 }
 
+// WithMaxBaseRPCPrice sets the max base RPC price for the host query
 func (hf HostFilter) WithMaxBaseRPCPrice(price types.Currency) {
 	hf["maxbaserpcprice"] = []string{price.String()}
 }
 
+// WithSectorAccessPrice sets the sector access price for the host query
 func (hf HostFilter) WithSectorAccessPrice(price types.Currency) {
 	hf["maxsectoraccessprice"] = []string{price.String()}
+}
+
+// WithSort sets the sort order for the host's query
+func (hf HostFilter) WithSort(field HostSort, desc bool) {
+	hf["sort"] = []string{string(field)}
+	if desc {
+		hf["desc"] = []string{"desc"}
+	} else {
+		hf["desc"] = []string{"asc"}
+	}
 }
 
 // GetNetworkAverages gets the average settings and benchmarks of all active hosts on the network
