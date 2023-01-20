b55a0c7ffc24d11c2d842c498a811b3d609ab96a
Nate Maninger <me@n8m.us>
2023-01-20 12:45:01 -0700

    fix sort

--- sia/hosts.go
+++ sia/hosts.go
@@ -1,17 +1,143 @@
 package sia
 
 import (
+	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
 	"strconv"
+	"time"
 
-	siamods "go.sia.tech/siad/modules"
 	"go.sia.tech/siad/types"
 )
 
+// UniqueID is a unique identifier
+type UniqueID types.Specifier
+
+// MarshalJSON marshals an id as a hex string.
+func (uid UniqueID) MarshalJSON() ([]byte, error) {
+	return json.Marshal(uid.String())
+}
+
+// String prints the uid in hex.
+func (uid UniqueID) String() string {
+	return hex.EncodeToString(uid[:])
+}
+
 type (
+	// RPCPriceTable contains the cost of executing a RPC on a host. Each host can
+	// set its own prices for the individual MDM instructions and RPC costs.
+	RPCPriceTable struct {
+		// UID is a specifier that uniquely identifies this price table
+		UID UniqueID `json:"uid"`
+
+		// Validity is a duration that specifies how long the host guarantees these
+		// prices for and are thus considered valid.
+		Validity time.Duration `json:"validity"`
+
+		// HostBlockHeight is the block height of the host. This allows the renter
+		// to create valid withdrawal messages in case it is not synced yet.
+		HostBlockHeight types.BlockHeight `json:"hostblockheight"`
+
+		// UpdatePriceTableCost refers to the cost of fetching a new price table
+		// from the host.
+		UpdatePriceTableCost types.Currency `json:"updatepricetablecost"`
+
+		// AccountBalanceCost refers to the cost of fetching the balance of an
+		// ephemeral account.
+		AccountBalanceCost types.Currency `json:"accountbalancecost"`
+
+		// FundAccountCost refers to the cost of funding an ephemeral account on the
+		// host.
+		FundAccountCost types.Currency `json:"fundaccountcost"`
+
+		// LatestRevisionCost refers to the cost of asking the host for the latest
+		// revision of a contract.
+		// TODO: should this be free?
+		LatestRevisionCost types.Currency `json:"latestrevisioncost"`
+
+		// SubscriptionMemoryCost is the cost of storing a byte of data for
+		// SubscriptionPeriod time.
+		SubscriptionMemoryCost types.Currency `json:"subscriptionmemorycost"`
+
+		// SubscriptionNotificationCost is the cost of a single notification on top
+		// of what is charged for bandwidth.
+		SubscriptionNotificationCost types.Currency `json:"subscriptionnotificationcost"`
+
+		// MDM related costs
+		//
+		// InitBaseCost is the amount of cost that is incurred when an MDM program
+		// starts to run. This doesn't include the memory used by the program data.
+		// The total cost to initialize a program is calculated as
+		// InitCost = InitBaseCost + MemoryTimeCost * Time
+		InitBaseCost types.Currency `json:"initbasecost"`
+
+		// MemoryTimeCost is the amount of cost per byte per time that is incurred
+		// by the memory consumption of the program.
+		MemoryTimeCost types.Currency `json:"memorytimecost"`
+
+		// Cost values specific to the bandwidth consumption.
+		DownloadBandwidthCost types.Currency `json:"downloadbandwidthcost"`
+		UploadBandwidthCost   types.Currency `json:"uploadbandwidthcost"`
+
+		// Cost values specific to the DropSectors instruction.
+		DropSectorsBaseCost types.Currency `json:"dropsectorsbasecost"`
+		DropSectorsUnitCost types.Currency `json:"dropsectorsunitcost"`
+
+		// Cost values specific to the HasSector command.
+		HasSectorBaseCost types.Currency `json:"hassectorbasecost"`
+
+		// Cost values specific to the Read instruction.
+		ReadBaseCost   types.Currency `json:"readbasecost"`
+		ReadLengthCost types.Currency `json:"readlengthcost"`
+
+		// Cost values specific to the RenewContract instruction.
+		RenewContractCost types.Currency `json:"renewcontractcost"`
+
+		// Cost values specific to the Revision command.
+		RevisionBaseCost types.Currency `json:"revisionbasecost"`
+
+		// SwapSectorCost is the cost of swapping 2 full sectors by root.
+		SwapSectorCost types.Currency `json:"swapsectorcost"`
+
+		// Cost values specific to the Write instruction.
+		WriteBaseCost   types.Currency `json:"writebasecost"`   // per write
+		WriteLengthCost types.Currency `json:"writelengthcost"` // per byte written
+		WriteStoreCost  types.Currency `json:"writestorecost"`  // per byte / block of additional storage
+
+		// TxnFee estimations.
+		TxnFeeMinRecommended types.Currency `json:"txnfeeminrecommended"`
+		TxnFeeMaxRecommended types.Currency `json:"txnfeemaxrecommended"`
+
+		// ContractPrice is the additional fee a host charges when forming/renewing
+		// a contract to cover the miner fees when submitting the contract and
+		// revision to the blockchain.
+		ContractPrice types.Currency `json:"contractprice"`
+
+		// CollateralCost is the amount of money per byte the host is promising to
+		// lock away as collateral when adding new data to a contract. It's paid out
+		// to the host regardless of the outcome of the storage proof.
+		CollateralCost types.Currency `json:"collateralcost"`
+
+		// MaxCollateral is the maximum amount of collateral the host is willing to
+		// put into a single file contract.
+		MaxCollateral types.Currency `json:"maxcollateral"`
+
+		// MaxDuration is the max duration for which the host is willing to form a
+		// contract.
+		MaxDuration types.BlockHeight `json:"maxduration"`
+
+		// WindowSize is the minimum time in blocks the host requests the
+		// renewWindow of a new contract to be.
+		WindowSize types.BlockHeight `json:"windowsize"`
+
+		// Registry related fields.
+		RegistryEntriesLeft  uint64 `json:"registryentriesleft"`
+		RegistryEntriesTotal uint64 `json:"registryentriestotal"`
+	}
+
 	getHostsResp struct {
 		APIResponse
 		Hosts []HostDetails `json:"hosts"`
@@ -24,10 +150,10 @@ type (
 
 	getAveragesResp struct {
 		APIResponse
-		Settings       HostConfig            `json:"settings"`
-		PriceTable     siamods.RPCPriceTable `json:"price_table"`
-		Benchmarks     AvgHostBenchmark      `json:"benchmarks"`
-		BenchmarksRHP2 AvgHostBenchmark      `json:"benchmarks_rhp2"`
+		Settings       HostConfig       `json:"settings"`
+		PriceTable     RPCPriceTable    `json:"price_table"`
+		Benchmarks     AvgHostBenchmark `json:"benchmarks"`
+		BenchmarksRHP2 AvgHostBenchmark `json:"benchmarks_rhp2"`
 	}
 
 	HostFilter url.Values
@@ -132,9 +258,9 @@ func (hf HostFilter) WithSectorAccessPrice(price types.Currency) {
 func (hf HostFilter) WithSort(field HostSort, desc bool) {
 	hf["sort"] = []string{string(field)}
 	if desc {
-		hf["desc"] = []string{"desc"}
+		hf["dir"] = []string{"desc"}
 	} else {
-		hf["desc"] = []string{"asc"}
+		hf["dir"] = []string{"asc"}
 	}
 }
 
--- sia/types.go
+++ sia/types.go
@@ -135,27 +135,28 @@ type (
 
 	//StorageContract a storage contract on the blockchain
 	StorageContract struct {
-		ID                     string          `json:"id"`
-		BlockID                string          `json:"block_id"`
-		TransactionID          string          `json:"transaction_id"`
-		MerkleRoot             string          `json:"merkle_root"`
-		UnlockHash             string          `json:"unlock_hash"`
-		Status                 string          `json:"status"`
-		RevisionNumber         uint64          `json:"revision_number"`
-		NegotiationHeight      uint64          `json:"negotiation_height"`
-		ExpirationHeight       uint64          `json:"expiration_height"`
-		ProofDeadline          uint64          `json:"proof_deadline"`
-		ProofHeight            uint64          `json:"proof_height"`
-		Payout                 types.Currency  `json:"payout"`
-		FileSize               types.Currency  `json:"file_size"`
-		ValidProofOutputs      []SiacoinOutput `json:"valid_proof_outputs"`
-		MissedProofOutputs     []SiacoinOutput `json:"missed_proof_outputs"`
-		NegotiationTimestamp   time.Time       `json:"negotiation_timestamp"`
-		ExpirationTimestamp    time.Time       `json:"expiration_timestamp"`
-		ProofDeadlineTimestamp time.Time       `json:"proof_deadline_timestamp"`
-		ProofTimestamp         time.Time       `json:"proof_timestamp"`
-		ProofConfirmed         bool            `json:"proof_confirmed"`
-		Unused                 bool            `json:"unused"`
+		ID                     string            `json:"id"`
+		BlockID                string            `json:"block_id"`
+		TransactionID          string            `json:"transaction_id"`
+		MerkleRoot             string            `json:"merkle_root"`
+		UnlockHash             string            `json:"unlock_hash"`
+		Status                 string            `json:"status"`
+		RevisionNumber         uint64            `json:"revision_number"`
+		NegotiationHeight      uint64            `json:"negotiation_height"`
+		ExpirationHeight       uint64            `json:"expiration_height"`
+		ProofDeadline          uint64            `json:"proof_deadline"`
+		ProofHeight            uint64            `json:"proof_height"`
+		Payout                 types.Currency    `json:"payout"`
+		FileSize               types.Currency    `json:"file_size"`
+		ValidProofOutputs      []SiacoinOutput   `json:"valid_proof_outputs"`
+		MissedProofOutputs     []SiacoinOutput   `json:"missed_proof_outputs"`
+		NegotiationTimestamp   time.Time         `json:"negotiation_timestamp"`
+		ExpirationTimestamp    time.Time         `json:"expiration_timestamp"`
+		ProofDeadlineTimestamp time.Time         `json:"proof_deadline_timestamp"`
+		ProofTimestamp         time.Time         `json:"proof_timestamp"`
+		ProofConfirmed         bool              `json:"proof_confirmed"`
+		Unused                 bool              `json:"unused"`
+		PreviousRevisions      []StorageContract `json:"previous_revisions"`
 	}
 
 	//StorageProof a storage proof on the blockchain
