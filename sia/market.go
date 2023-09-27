4ac0cebb4f262b262202b2507406c89a8725600b
Nate Maninger <me@n8m.us>
2023-09-27 11:37:57 -0600

    add historical exchange rate

--- sia/market.go
+++ sia/market.go
@@ -3,6 +3,8 @@ package sia
 import (
 	"errors"
 	"net/http"
+	"net/url"
+	"time"
 )
 
 type getPriceResp struct {
@@ -11,7 +13,7 @@ type getPriceResp struct {
 	Siafund map[string]float64 `json:"siafund"`
 }
 
-//GetExchangeRate gets the current market exchange rate for Siacoin and Siafund
+// GetExchangeRate gets the current market exchange rate for Siacoin and Siafund
 func (a *APIClient) GetExchangeRate() (siacoin map[string]float64, siafund map[string]float64, err error) {
 	var resp getPriceResp
 
@@ -30,4 +32,25 @@ func (a *APIClient) GetExchangeRate() (siacoin map[string]float64, siafund map[s
 	siafund = resp.Siafund
 
 	return
+}
+
+// GetHistoricalExchangeRate gets the historical market exchange rate for
+// Siacoins at the specified timestamp
+func (a *APIClient) GetHistoricalExchangeRate(timestamp time.Time) (map[string]float64, error) {
+	var resp getPriceResp
+
+	v := url.Values{
+		"timestamp": []string{timestamp.Format(time.RFC3339)},
+	}
+
+	code, err := a.makeAPIRequest(http.MethodGet, "/market/exchange-rate/historical?"+v.Encode(), nil, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	if code < 200 || code >= 300 || resp.Type != "success" {
+		return nil, errors.New(resp.Message)
+	}
+
+	return resp.Siacoin, nil
 }
