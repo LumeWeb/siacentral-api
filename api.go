4ff40c07bce5abe46ff6c491c2e1230b87ceead6
Derrick Hammer <derrick@derrickhammer.com>
2024-03-11 07:41:01 -0400

    chore: update module name

--- api.go
+++ api.go
@@ -1,8 +1,6 @@
 package apisdkgo
 
-import (
-	"github.com/siacentral/apisdkgo/sia"
-)
+import "github.com/LumeWeb/siacentral-api/sia"
 
 // NewSiaClient intializes a new Sia Central API client
 func NewSiaClient() *sia.APIClient {
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/siacentral/apisdkgo
+module github.com/LumeWeb/siacentral-api
 
 go 1.19
 
